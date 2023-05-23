ba7f9f3303482e1b7a08739d5aef2b733ee93ddd
alvine2200 <alvinellavu@gmail.com>
2023-05-23 16:51:39 +0300

    flutter themes

---
+++ lib/themes.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ThemesPage extends StatelessWidget {
+  const ThemesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.light),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      home: const HomeApp(),
+    );
+  }
+}
+
+class HomeApp extends StatefulWidget {
+  const HomeApp({super.key});
+
+  @override
+  State<HomeApp> createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
