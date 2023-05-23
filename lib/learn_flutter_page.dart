5f82be04718628bc124d3a5a19b375ddde8be621
alvine2200 <alvinellavu@gmail.com>
2023-05-23 11:02:08 +0300

    checkbox done

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -9,6 +9,7 @@ class LearnFlutterPage extends StatefulWidget {
 
 class _LearnFlutterPageState extends State<LearnFlutterPage> {
   bool isSwitch = false;
+  bool? isCheckBox = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,69 +23,85 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           },
         ),
       ),
-      body: Column(
-        children: [
-          Image.asset(
-            'images/sky.jpeg',
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          const Divider(
-            color: Colors.black,
-          ),
-          Container(
-            color: Colors.blueGrey,
-            margin: const EdgeInsets.all(10.0),
-            padding: const EdgeInsets.all(10),
-            child: const Center(
-              child: Text(
-                'Testing Container',
-                style: TextStyle(color: Colors.white),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              'images/sky.jpeg',
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              color: Colors.black,
+            ),
+            Container(
+              color: Colors.blueGrey,
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10),
+              child: const Center(
+                child: Text(
+                  'Testing Container',
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              debugPrint('elevated button shit');
-            },
-            child: const Text('Elevated Button'),
-          ),
-          OutlinedButton(
-            onPressed: () {
-              debugPrint('Outlined button shit');
-            },
-            child: const Text('Outlined Button'),
-          ),
-          TextButton(
-            onPressed: () {
-              debugPrint('Text button shit');
-            },
-            child: const Text('Text Button'),
-          ),
-          GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () {
-              debugPrint('This is the row');
-            },
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(Icons.local_fire_department),
-                Text('footer page'),
-                Icon(Icons.local_fire_department),
-              ],
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: isSwitch
+                      ? const Color.fromARGB(255, 218, 35, 50)
+                      : Colors.green),
+              onPressed: () {
+                debugPrint('elevated button shit');
+              },
+              child: const Text('Elevated Button'),
             ),
-          ),
-          Switch(
-            value: isSwitch,
-            onChanged: (bool newBool) {
-              setState(() {
-                isSwitch = newBool;
-              });
-            },
-          )
-        ],
+            OutlinedButton(
+              onPressed: () {
+                debugPrint('Outlined button shit');
+              },
+              child: const Text('Outlined Button'),
+            ),
+            TextButton(
+              onPressed: () {
+                debugPrint('Text button shit');
+              },
+              child: const Text('Text Button'),
+            ),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                debugPrint('This is the row');
+              },
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(Icons.local_fire_department),
+                  Text('footer page'),
+                  Icon(Icons.local_fire_department),
+                ],
+              ),
+            ),
+            Switch(
+              value: isSwitch,
+              onChanged: (bool newBool) {
+                setState(() {
+                  isSwitch = newBool;
+                });
+              },
+            ),
+            Checkbox(
+              value: isCheckBox,
+              onChanged: (bool? newBool) {
+                setState(() {
+                  isCheckBox = newBool;
+                });
+              },
+            ),
+            Image.network(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUp2PuFbiZB1GyqXK4GMUI6xOE84QOL0fPsqxOetht3tg1ZnSHR0UTcDDhRc7qCznK9pE&usqp=CAU')
+          ],
+        ),
       ),
     );
   }
