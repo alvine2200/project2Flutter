0f86440ad0478294cbf5b4ef5dd2fbfb957b0006
alvine2200 <alvinellavu@gmail.com>
2023-05-23 09:46:47 +0300

    images

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -13,6 +13,24 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Learn Flutter'),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: Column(
+        children: [
+          Image.asset('images/sky.jpeg'),
+          const SizedBox(
+            height: 10,
+          ),
+          const Divider(
+            color: Colors.black,
+          ),
+        ],
       ),
     );
   }
